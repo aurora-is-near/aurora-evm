a87dd22e4adf591cb4e999d0a1d324a61263f65e
Evgeny Ukhanov <evgeny@aurora.dev>
2025-01-22 18:00:19 +0100

    Added G1 point

--- evm-tests/ethcore-builtin/src/bls.rs
+++ evm-tests/ethcore-builtin/src/bls.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use blst::{blst_bendian_from_fp, blst_fp};
+use blst::{blst_bendian_from_fp, blst_fp, blst_p1_affine};
 
 /// Number of bits used in the BLS12-381 curve finite field elements.
 const NBITS: usize = 256;
@@ -20,12 +20,29 @@ const MODULUS_REPR: [u8; 48] = [
 	0x1e, 0xab, 0xff, 0xfe, 0xb1, 0x53, 0xff, 0xff, 0xb9, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xaa, 0xab,
 ];
 
+/// Length of each of the elements in a g1 operation input.
+const G1_INPUT_ITEM_LENGTH: usize = 128;
+
+/// Output length of a g1 operation.
+const G1_OUTPUT_LENGTH: usize = 128;
+
 /// BLS Encodes a single finite field element into byte slice with padding.
-pub fn x(out: &mut [u8], input: *const blst_fp) {
+fn fp_to_bytes(out: &mut [u8], input: *const blst_fp) {
 	if out.len() != PADDED_FP_LENGTH {
 		return;
 	}
 	let (padding, rest) = out.split_at_mut(PADDING_LENGTH);
 	padding.fill(0);
 	unsafe { blst_bendian_from_fp(rest.as_mut_ptr(), input) };
+}
+
+/// Encodes a G1 point in affine format into byte slice with padded elements.
+pub fn encode_g1_point(input: *const blst_p1_affine) -> Vec<u8> {
+	let mut out = vec![0u8; G1_OUTPUT_LENGTH];
+	// SAFETY: out comes from fixed length array, input is a blst value.
+	unsafe {
+		fp_to_bytes(&mut out[..PADDED_FP_LENGTH], &(*input).x);
+		fp_to_bytes(&mut out[PADDED_FP_LENGTH..], &(*input).y);
+	}
+	out
 }
