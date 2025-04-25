e38a27c73ee26f00ec24dbe99f791c9b744032a7
Evgeny Ukhanov <evgeny@aurora.dev>
2025-04-25 12:44:30 +0200

    Added PostState logic

--- evm-tests/jsontests/src/types/json_utils.rs
+++ evm-tests/jsontests/src/types/json_utils.rs
@@ -1,19 +1,17 @@
 use primitive_types::{H160, H256, U256};
 use serde::de::Error;
 use serde::{Deserialize, Deserializer};
+use std::collections::BTreeMap;
 use std::str::FromStr;
 
 /// Removes the "0x" prefix from a string if it exists.
-fn strip_prefix(s: &str) -> &str {
+pub fn strip_0x_prefix(s: &str) -> &str {
     s.strip_prefix("0x").unwrap_or(s)
 }
 
 /// Converts a hexadecimal string into a u64 value.
 /// Returns an error if the string length is greater than 16 or parsing fails.
-fn u64_from_str<'de, D>(value: &str) -> Result<u64, D::Error>
-where
-    D: Deserializer<'de>,
-{
+fn u64_from_str<'de, D: Deserializer<'de>>(value: &str) -> Result<u64, D::Error> {
     if value.len() > 16 {
         return Err(Error::custom(
             format!("u64 value too big (length={})", value.len()).as_str(),
@@ -24,12 +22,9 @@ where
         .map_err(|e| Error::custom(format!("Invalid u64 value: {e}").as_str()))
 }
 
-/// Converts a hexadecimal string into a U256 value.
+/// Converts a hexadecimal string into a `U256` value.
 /// Returns an error if the string length is greater than 64 or parsing fails.
-fn u256_from_str<'de, D>(value: &str) -> Result<U256, D::Error>
-where
-    D: Deserializer<'de>,
-{
+fn u256_from_str<'de, D: Deserializer<'de>>(value: &str) -> Result<U256, D::Error> {
     if value.len() > 64 {
         return Err(Error::custom(
             format!("U256 value too big (length={})", value.len()).as_str(),
@@ -40,14 +35,9 @@ where
         .map_err(|e| Error::custom(format!("Invalid U256 value: {e}").as_str()))
 }
 
-/// Deserializes a hexadecimal string into an H160 address.
+/// Converts a hexadecimal string into a `H160` value.
 /// Returns an error if the string length is greater than 40 or parsing fails.
-pub fn deserialize_h160_from_str<'de, D>(deserializer: D) -> Result<H160, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    let value = strip_prefix(&s);
+pub fn h160_from_str<'de, D: Deserializer<'de>>(value: &str) -> Result<H160, D::Error> {
     if value.len() > 40 {
         return Err(Error::custom(
             format!("H160 value too big (length={})", value.len()).as_str(),
@@ -56,66 +46,117 @@ where
     H160::from_str(value).map_err(|e| Error::custom(format!("Invalid H160 value: {e}").as_str()))
 }
 
+/// Converts a `BTreeMap` with hexadecimal string keys and values into a `BTreeMap` with `U256` keys and values.
+/// The hexadecimal strings may optionally start with the "0x" prefix.
+/// Returns an error if any key or value cannot be parsed into a U256.
+fn btree_u256_u256_from_str<'de, D: Deserializer<'de>>(
+    map_str: BTreeMap<String, String>,
+) -> Result<BTreeMap<U256, U256>, D::Error> {
+    let mut map = BTreeMap::new();
+    for (k, v) in map_str {
+        let key = u256_from_str::<D>(strip_0x_prefix(&k))?;
+        let value = u256_from_str::<D>(strip_0x_prefix(&v))?;
+        map.insert(key, value);
+    }
+    Ok(map)
+}
+
+/// Deserializes a hexadecimal string into an H160 address.
+/// Returns an error if the string length is greater than 40 or parsing fails.
+pub fn deserialize_h160_from_str<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<H160, D::Error> {
+    h160_from_str::<D>(strip_0x_prefix(&String::deserialize(deserializer)?))
+}
+
 /// Deserializes a hexadecimal string into an H256 hash by converting it to U256 first.
 /// Returns an error if parsing fails.
-#[allow(dead_code)]
-pub fn deserialize_h256_from_u256_str<'de, D>(deserializer: D) -> Result<H256, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let v = u256_from_str::<D>(strip_prefix(&String::deserialize(deserializer)?))?;
+pub fn deserialize_h256_from_u256_str<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<H256, D::Error> {
+    let v = u256_from_str::<D>(strip_0x_prefix(&String::deserialize(deserializer)?))?;
     Ok(H256::from(v.to_big_endian()))
 }
 
 /// Deserializes a hexadecimal string into a U256 value.
 /// Returns an error if parsing fails.
-pub fn deserialize_u256_from_str<'de, D>(deserializer: D) -> Result<U256, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    u256_from_str::<D>(strip_prefix(&String::deserialize(deserializer)?))
+pub fn deserialize_u256_from_str<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<U256, D::Error> {
+    u256_from_str::<D>(strip_0x_prefix(&String::deserialize(deserializer)?))
 }
 
 /// Deserializes a hexadecimal string into a u64 value.
 /// Returns an error if parsing fails.
 #[allow(dead_code)]
-pub fn deserialize_u64_from_str<'de, D>(deserializer: D) -> Result<u64, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    u64_from_str::<D>(strip_prefix(&String::deserialize(deserializer)?))
+pub fn deserialize_u64_from_str<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<u64, D::Error> {
+    u64_from_str::<D>(strip_0x_prefix(&String::deserialize(deserializer)?))
 }
 
 /// Deserializes an optional hexadecimal string into an optional H256 hash.
 /// Returns None if the value is missing.
-pub fn deserialize_h256_from_u256_str_opt<'de, D>(deserializer: D) -> Result<Option<H256>, D::Error>
-where
-    D: Deserializer<'de>,
-{
+pub fn deserialize_h256_from_u256_str_opt<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<H256>, D::Error> {
     Option::<String>::deserialize(deserializer)?
-        .map(|s| u256_from_str::<D>(strip_prefix(&s)).map(|v| H256::from(v.to_big_endian())))
+        .map(|s| u256_from_str::<D>(strip_0x_prefix(&s)).map(|v| H256::from(v.to_big_endian())))
         .transpose()
 }
 
 /// Deserializes an optional hexadecimal string into an optional U256 value.
 /// Returns None if the value is missing.
 #[allow(dead_code)]
-pub fn deserialize_u256_from_str_opt<'de, D>(deserializer: D) -> Result<Option<U256>, D::Error>
-where
-    D: Deserializer<'de>,
-{
+pub fn deserialize_u256_from_str_opt<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<U256>, D::Error> {
     Option::<String>::deserialize(deserializer)?
-        .map(|s| u256_from_str::<D>(strip_prefix(&s)))
+        .map(|s| u256_from_str::<D>(strip_0x_prefix(&s)))
         .transpose()
 }
 
 /// Deserializes an optional hexadecimal string into an optional u64 value.
 /// Returns None if the value is missing.
-pub fn deserialize_u64_from_str_opt<'de, D>(deserializer: D) -> Result<Option<u64>, D::Error>
-where
-    D: Deserializer<'de>,
-{
+pub fn deserialize_u64_from_str_opt<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<u64>, D::Error> {
     Option::<String>::deserialize(deserializer)?
-        .map(|s| u64_from_str::<D>(strip_prefix(&s)))
+        .map(|s| u64_from_str::<D>(strip_0x_prefix(&s)))
+        .transpose()
+}
+
+/// Deserializes hexadecimal string into vector of bytes.
+/// The hexadecimal string may start with the "0x" prefix.
+pub fn deserialize_bytes_from_str<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Vec<u8>, D::Error> {
+    let s = String::deserialize(deserializer)?;
+    hex::decode(strip_0x_prefix(&s)).map_err(|e: hex::FromHexError| Error::custom(e.to_string()))
+}
+
+/// Deserializes an optional hexadecimal string into an optional vector of bytes.
+/// The hexadecimal string may start with the "0x" prefix.
+/// Returns None if the value is missing.
+pub fn deserialize_bytes_from_str_opt<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<Vec<u8>>, D::Error> {
+    Option::deserialize(deserializer)?
+        .map(|s| {
+            hex::decode(strip_0x_prefix(s))
+                .map_err(|e: hex::FromHexError| Error::custom(e.to_string()))
+        })
+        .transpose()
+}
+
+/// Deserializes an optional `JSON` object with hexadecimal string keys and values into an optional
+/// `BTreeMap` with `U256` keys and values.
+/// The hexadecimal strings may start with the "0x" prefix.
+/// Returns None if the value is missing.
+pub fn deserialize_btree_u256_u256_from_str_opt<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<Option<BTreeMap<U256, U256>>, D::Error> {
+    Option::<BTreeMap<String, String>>::deserialize(deserializer)?
+        .map(btree_u256_u256_from_str::<D>)
         .transpose()
 }
--- evm-tests/jsontests/src/types/mod.rs
+++ evm-tests/jsontests/src/types/mod.rs
@@ -1,28 +1,32 @@
 use crate::types::json_utils::{
-    deserialize_h160_from_str, deserialize_h256_from_u256_str_opt, deserialize_u256_from_str,
-    deserialize_u64_from_str_opt,
+    deserialize_btree_u256_u256_from_str_opt, deserialize_bytes_from_str,
+    deserialize_bytes_from_str_opt, deserialize_h160_from_str, deserialize_h256_from_u256_str,
+    deserialize_h256_from_u256_str_opt, deserialize_u256_from_str, deserialize_u64_from_str_opt,
+    h160_from_str, strip_0x_prefix,
 };
 use primitive_types::{H160, H256, U256};
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
+use std::collections::BTreeMap;
 
 mod json_utils;
 
 /// Represents a test case for the Ethereum state transitions.
 /// It includes the environment setup, pre-state, transaction details,
 /// expected post-state results for different forks, configuration, and metadata.
-#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[derive(Debug, Eq, PartialEq, Deserialize)]
 pub struct StateTestCase {
     /// The environment parameters for the state test execution.
     /// This includes block-specific information like coinbase address, difficulty, gas limit, etc.
     pub env: StateEnv,
+
+    /// The initial state of accounts before the transaction is executed.
+    #[serde(rename = "pre")]
+    pub pre_state: PreState,
     /*
-        /// The initial state of accounts before the transaction is executed.
-        #[serde(rename = "pre")]
-        pub pre_state: AccountState,
         /// The expected state of accounts after the transaction execution for various forks.
         /// Maps fork specifications to a list of possible outcomes (results).
         #[serde(rename = "post")]
-        pub post_states: BTreeMap<ForkSpec, Vec<PostStateResult>>,
+        pub post_states: BTreeMap<ForkSpec, Vec<PostState>>,
         /// The transaction(s) to be executed in the test case.
         /// Can represent different transaction types across forks.
         pub transaction: MultiTransaction,
@@ -36,8 +40,8 @@ pub struct StateTestCase {
 
 /// Represents the environment parameters under which a state test is executed.
 /// These parameters typically correspond to the fields of a block header.
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Debug, Eq, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
 pub struct StateEnv {
     /// The address of the beneficiary account (miner) to whom the block rewards are transferred.
     #[serde(
@@ -96,4 +100,76 @@ pub struct StateEnv {
     /// The excess blob gas for the current block being processed. Relevant for EIP-4844.
     #[serde(deserialize_with = "deserialize_u64_from_str_opt")]
     pub current_excess_blob_gas: Option<u64>,
+}
+
+/// `PreState` represents a sorted mapping from Ethereum account addresses (`H160`) to their
+/// corresponding state (`StateAccount`).
+/// Represents vis `AccountsState`.
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Deserialize)]
+pub struct PreState(AccountsState);
+
+/// `AccountsState` represents a sorted mapping from Ethereum account addresses (`H160`) to their
+/// corresponding state (`StateAccount`).
+/// It uses a `BTreeMap` to ensure a consistent order for serialization.
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub struct AccountsState(BTreeMap<H160, StateAccount>);
+
+impl<'de> Deserialize<'de> for AccountsState {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let map: BTreeMap<String, StateAccount> = Deserialize::deserialize(deserializer)?;
+        let mut inner = BTreeMap::new();
+        for (k, v) in map {
+            let address = h160_from_str::<D>(strip_0x_prefix(&k))?;
+            inner.insert(address, v);
+        }
+        Ok(Self(inner))
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase", deny_unknown_fields)]
+pub struct PostState {
+    /// Post state hash
+    #[serde(deserialize_with = "deserialize_h256_from_u256_str")]
+    pub hash: H256,
+    /// Post state logs
+    #[serde(deserialize_with = "deserialize_h256_from_u256_str")]
+    pub logs: H256,
+    /// Indexes
+    pub indexes: PostStateIndexes,
+    /// Expected error if the test is meant to fail
+    pub expect_exception: Option<String>,
+    /// Transaction bytes
+    #[serde(rename = "txbytes", deserialize_with = "deserialize_bytes_from_str")]
+    pub tx_bytes: Vec<u8>,
+    /// Accounts state
+    pub state: Option<AccountsState>,
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct StateAccount {
+    /// Account Nonce.
+    #[serde(deserialize_with = "deserialize_u256_from_str")]
+    pub nonce: U256,
+    /// Account Balance.
+    #[serde(deserialize_with = "deserialize_u256_from_str")]
+    pub balance: U256,
+    /// Account Code.
+    #[serde(deserialize_with = "deserialize_bytes_from_str_opt")]
+    pub code: Option<Vec<u8>>,
+    /// Account Storage.
+    #[serde(deserialize_with = "deserialize_btree_u256_u256_from_str_opt")]
+    pub storage: Option<BTreeMap<U256, U256>>,
+}
+
+/// Post State indexes.
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+pub struct PostStateIndexes {
+    /// Index into transaction data set.
+    pub data: u64,
+    /// Index into transaction gas limit set.
+    pub gas: u64,
+    /// Index into transaction value set.
+    pub value: u64,
 }
